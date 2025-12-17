f04d2bccfbc6a3c7a1b9d317fda673fbc4d952d7
claurine <laurine.camuset@ynov.com>
2025-12-17 08:46:22 +0100

    feat(frontend/base) initialized frontend base with header, footer and recepies content with mock data!

---
+++ frontend/src/components/common/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <footer className="mt-auto" style={{backgroundColor: '#095d63'}}>
+            <div className="container mx-auto px-4 py-6">
+                <div className="text-center text-white">
+                    <p className="text-sm">
+                        Made By Mattéo et Laurine - 2025 Yboouf
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
